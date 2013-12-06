5cf0a4da9936941fd3fc374cebb22075becd868a
denisdbv <denisdbv@gmail.com>
2013-12-06 13:42:38 +0400

    Init App

---
+++ ParlamentApp/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  ParlamentApp
+//
+//  Created by DenisDbv on 06.12.13.
+//  Copyright (c) 2013 brandmill. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "PMAppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([PMAppDelegate class]));
+    }
+}
---
+++ ParlamentAppTests/ParlamentAppTests.m
@@ -0,0 +1,34 @@
+//
+//  ParlamentAppTests.m
+//  ParlamentAppTests
+//
+//  Created by DenisDbv on 06.12.13.
+//  Copyright (c) 2013 brandmill. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface ParlamentAppTests : XCTestCase
+
+@end
+
+@implementation ParlamentAppTests
+
+- (void)setUp
+{
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown
+{
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    XCTFail(@"No implementation for \"%s\"", __PRETTY_FUNCTION__);
+}
+
+@end
